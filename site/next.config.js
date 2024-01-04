c0d494503f1efd727bfe4a283857ec556cc0ab8d
ChiYuFeng <hongqt@lsnu.edu.cn>
2024-01-04 10:01:33 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Lsnu Technologies',
+    description: 'The official store for Lsnu-Linux workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/hongqt/LsnuLinux/',
+    contactUrl: 'https://hongqt.lsnu.edu.cn',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
